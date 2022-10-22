c64d5fa04a54ff649b44439fe036b7b8bc2b3569
agilhz <agilhz15@gmail.com>
2022-10-22 21:23:01 +0700

    update perubahan pada bagian portofolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// refresh
+window.onresize = function () {
+  location.reload();
+};
+
 // Navbar
 
 const navbar = document.getElementById("navbar");
@@ -31,3 +36,51 @@ exit.addEventListener("click", function () {
   pricing.style.display = "none";
   mask.style.display = "none";
 });
+
+// carousel auto play
+
+const imageWrapper = document.querySelector(".image-wrapper");
+const imageItems = document.querySelectorAll(".image-wrapper > *");
+const imageLength = imageItems.length;
+const perView = view();
+let totalScroll = 0;
+const delay = 2000;
+
+function view() {
+  if (window.innerWidth > 1024) {
+    return 3;
+  } else if (window.innerWidth > 768) {
+    return 2;
+  } else {
+    return 1;
+  }
+}
+
+imageWrapper.style.setProperty("--per-view", perView);
+for (let i = 0; i < perView; i++) {
+  imageWrapper.insertAdjacentHTML("beforeend", imageItems[i].outerHTML);
+}
+
+let autoScroll = setInterval(scrolling, delay);
+
+imageWrapper.addEventListener("mouseover", function () {
+  clearInterval(autoScroll);
+});
+
+imageWrapper.addEventListener("mouseout", function () {
+  autoScroll = setInterval(scrolling, delay);
+});
+
+function scrolling() {
+  totalScroll++;
+  if (totalScroll == imageLength + 1) {
+    clearInterval(autoScroll);
+    totalScroll = 1;
+    imageWrapper.style.transition = "0s";
+    imageWrapper.style.left = "0";
+    autoScroll = setInterval(scrolling, delay);
+  }
+  const widthEl = document.querySelector(".image-wrapper > :first-child").offsetWidth + 24;
+  imageWrapper.style.left = `-${totalScroll * widthEl}px`;
+  imageWrapper.style.transition = ".3s";
+}
